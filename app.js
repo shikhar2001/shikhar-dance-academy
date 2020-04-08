2a388780c62a6e0ac6619fe296db484a2076d57e
shikhar2001 <imt_2019095@iiitm.ac.in>
2020-04-08 12:44:06 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =require("express");
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 80; 
+process.env.PORT || 3000; 
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
